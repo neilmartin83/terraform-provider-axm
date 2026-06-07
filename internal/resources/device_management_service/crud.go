0dcf2981fb0a9f1db1596b8fcc0e6de0ab6a777b
Neil Martin <neilmartin83@icloud.com>
2026-06-07 10:40:16 +0100

    feat(device_management_service): add read-only MdmServer attributes to resource
    Add status, device_count, default_product_families,
    last_connected_date_time, last_connected_ip, created_date_time,
    and updated_date_time as computed-only fields on the resource.
    Populate them in Create, Read, and Update from the API response.

--- internal/resources/device_management_service/crud.go
+++ internal/resources/device_management_service/crud.go
@@ -73,6 +73,13 @@ func (r *DeviceManagementServiceResource) Create(ctx context.Context, req resour
 
 	data.ID = types.StringValue(srv.ID)
 	data.Type = types.StringValue(srv.Attributes.ServerType)
+	data.Status = types.StringValue(srv.Attributes.Status)
+	data.DeviceCount = types.Int64Value(int64(srv.Attributes.DeviceCount))
+	data.DefaultProductFamilies = common.StringsToTypesStrings(srv.Attributes.DefaultProductFamilies)
+	data.LastConnectedDateTime = types.StringValue(srv.Attributes.LastConnectedDateTime)
+	data.LastConnectedIp = types.StringValue(srv.Attributes.LastConnectedIp)
+	data.CreatedDateTime = types.StringValue(srv.Attributes.CreatedDateTime)
+	data.UpdatedDateTime = types.StringValue(srv.Attributes.UpdatedDateTime)
 	// AllowRelease is not reliably echoed by the create response; keep the plan value.
 	// Read will reconcile on the next refresh if Apple silently ignored it.
 
@@ -171,6 +178,13 @@ func (r *DeviceManagementServiceResource) Read(ctx context.Context, req resource
 
 	data.Name = types.StringValue(srv.Attributes.ServerName)
 	data.Type = types.StringValue(srv.Attributes.ServerType)
+	data.Status = types.StringValue(srv.Attributes.Status)
+	data.DeviceCount = types.Int64Value(int64(srv.Attributes.DeviceCount))
+	data.DefaultProductFamilies = common.StringsToTypesStrings(srv.Attributes.DefaultProductFamilies)
+	data.LastConnectedDateTime = types.StringValue(srv.Attributes.LastConnectedDateTime)
+	data.LastConnectedIp = types.StringValue(srv.Attributes.LastConnectedIp)
+	data.CreatedDateTime = types.StringValue(srv.Attributes.CreatedDateTime)
+	data.UpdatedDateTime = types.StringValue(srv.Attributes.UpdatedDateTime)
 	data.AllowRelease = types.BoolValue(srv.Attributes.EnableMdmDisownFlag)
 
 	deviceIDs, err := r.client.GetDeviceManagementServiceSerialNumbers(readCtx, data.ID.ValueString())
@@ -259,6 +273,13 @@ func (r *DeviceManagementServiceResource) Update(ctx context.Context, req resour
 				return
 			}
 			plan.Type = types.StringValue(srv.Attributes.ServerType)
+			plan.Status = types.StringValue(srv.Attributes.Status)
+			plan.DeviceCount = types.Int64Value(int64(srv.Attributes.DeviceCount))
+			plan.DefaultProductFamilies = common.StringsToTypesStrings(srv.Attributes.DefaultProductFamilies)
+			plan.LastConnectedDateTime = types.StringValue(srv.Attributes.LastConnectedDateTime)
+			plan.LastConnectedIp = types.StringValue(srv.Attributes.LastConnectedIp)
+			plan.CreatedDateTime = types.StringValue(srv.Attributes.CreatedDateTime)
+			plan.UpdatedDateTime = types.StringValue(srv.Attributes.UpdatedDateTime)
 			plan.AllowRelease = types.BoolValue(srv.Attributes.EnableMdmDisownFlag)
 		}
 	}
